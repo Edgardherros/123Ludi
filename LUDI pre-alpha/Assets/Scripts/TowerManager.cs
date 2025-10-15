7ba062bb8be4aa25f1345809e180dffd84dbbb98
MancosHD <73612486+MancosHD@users.noreply.github.com>
2025-10-15 18:26:35 +0200

    si

---
+++ LUDI pre-alpha/Assets/Scripts/TowerManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class TowerManager : MonoBehaviour
+{
+
+    [HideInInspector] public bool selectingTower;
+    
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        selectingTower = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
