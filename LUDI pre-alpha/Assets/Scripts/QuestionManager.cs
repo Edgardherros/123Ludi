5de2e2c735dab17adcee0cea9604b17c99188507
MancosHD <73612486+MancosHD@users.noreply.github.com>
2025-10-22 13:59:08 +0200

    preguntas ya no se repiten

--- LUDI pre-alpha/Assets/Scripts/QuestionManager.cs
+++ LUDI pre-alpha/Assets/Scripts/QuestionManager.cs
@@ -11,34 +11,75 @@ public class QuestionManager : MonoBehaviour
     [SerializeField] string[] semanticsQuestions;
     [SerializeField] string[] grammarQuestions;
     [SerializeField] string[] ortographyQuestions;
-    
-    
+
+    [HideInInspector] public bool[] answeredSemanticsQuestions;
+    [HideInInspector] public bool[] answeredGrammarQuestions;
+    [HideInInspector] public bool[] answeredOrtographyQuestions;
+
+    private void Start()
+    {
+        answeredSemanticsQuestions = new bool[semanticsQuestions.Length];
+        answeredGrammarQuestions = new bool[grammarQuestions.Length];
+        answeredOrtographyQuestions = new bool[ortographyQuestions.Length];
+    }
 
     public void setQuestion()
     {
         int chosenQuestion;
-
+        bool repeat;
         switch (towerManager.currentTower)
         {
             case TowerManager.TowerType.semantica:
                 
-                chosenQuestion = Random.Range(0, semanticsQuestions.Length);
-                question.text = semanticsQuestions[chosenQuestion];
-                answerManager.setAnswers(TowerManager.TowerType.semantica, chosenQuestion);
+                repeat = true;
+                while (repeat)
+                {
+                    chosenQuestion = Random.Range(0, semanticsQuestions.Length);
+                    question.text = semanticsQuestions[chosenQuestion];
+                    answerManager.setAnswers(TowerManager.TowerType.semantica, chosenQuestion);
+
+                    if (!answeredSemanticsQuestions[chosenQuestion])
+                    {
+                        answeredSemanticsQuestions[chosenQuestion] = true;
+                        repeat = false;
+                    }
+                }
+
 
                 break;
             case TowerManager.TowerType.lexico:
                 
-                chosenQuestion = Random.Range(0, grammarQuestions.Length);
-                question.text = grammarQuestions[chosenQuestion];
-                answerManager.setAnswers(TowerManager.TowerType.lexico, chosenQuestion);
+                repeat = true;
+                while (repeat)
+                {
+                    chosenQuestion = Random.Range(0, grammarQuestions.Length);
+                    question.text = grammarQuestions[chosenQuestion];
+                    answerManager.setAnswers(TowerManager.TowerType.lexico, chosenQuestion);
+
+                    if (!answeredGrammarQuestions[chosenQuestion])
+                    {
+                        answeredGrammarQuestions[chosenQuestion] = true;
+                        repeat = false;
+                    }
+                }
+
 
                 break;
             case TowerManager.TowerType.ortografia:
                 
-                chosenQuestion = Random.Range(0, ortographyQuestions.Length);
-                question.text = ortographyQuestions[chosenQuestion];
-                answerManager.setAnswers(TowerManager.TowerType.ortografia, chosenQuestion);
+                repeat = true;
+                while (repeat)
+                {
+                    chosenQuestion = Random.Range(0, ortographyQuestions.Length);
+                    question.text = ortographyQuestions[chosenQuestion];
+                    answerManager.setAnswers(TowerManager.TowerType.ortografia, chosenQuestion);
+
+                    if (!answeredOrtographyQuestions[chosenQuestion])
+                    {
+                        answeredOrtographyQuestions[chosenQuestion] = true;
+                        repeat = false;
+                    }
+                }
 
                 break;
         }
