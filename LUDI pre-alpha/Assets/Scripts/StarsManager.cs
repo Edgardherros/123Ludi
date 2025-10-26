b1b2b477b1bebaa3386fb4315cfc31c92eca86ac
Edgardhr <Edgard.hernandez@enti.cat>
2025-10-26 17:08:30 +0100

    Estrellas conseguidas funcional
    Se guardan las estrellas conseguidas de los niveles

--- LUDI pre-alpha/Assets/Scripts/StarsManager.cs
+++ LUDI pre-alpha/Assets/Scripts/StarsManager.cs
@@ -2,18 +2,26 @@ using UnityEngine;
 public class StarsManager : MonoBehaviour
 {    
     [SerializeField] public GameObject[] L1;
+    [SerializeField] public GameObject[] L0;
     void Start()
     {
-        PlayerPrefs.SetInt("Level1Stars", 3);
-        
         CheckLevel1();
-        
+        CheckLevel0();        
+        Debug.Log("Level 1 Stars: " + PlayerPrefs.GetInt("Level1Stars"));
+        Debug.Log("Level 0 Stars: " + PlayerPrefs.GetInt("Level0Stars"));
     }
     void CheckLevel1()
     {
-        for(int i = 0;i < PlayerPrefs.GetInt("Level1Stars"); i++)
+        for (int j = 0; j < PlayerPrefs.GetInt("Level1Stars"); j++)
         {
-            L1[i].SetActive(true);      
+            L1[j].SetActive(true);
+        }        
+    }
+    void CheckLevel0()
+    {
+        for (int j = 0; j < PlayerPrefs.GetInt("Level0Stars"); j++)
+        {
+            L0[j].SetActive(true);
         }
     }
 }
